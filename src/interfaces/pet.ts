fcb137a07798e8de15b13af9795a1e6da45ac45e
SuhSales <suelem_pira@hotmail.com>
2024-04-17 22:27:13 -0300

    Adicionando Skeleton load, http client, etc

---
+++ src/interfaces/pet.ts
@@ -0,0 +1,24 @@
+export interface IPet {
+    id: string
+    name: string
+    type: string
+    size: string
+    gender: string
+    bio: string
+    photo: string
+}
+
+export type GetPetsRequest = Partial<Pick <IPet, 'type' | 'size' | 'gender'>> & {
+    page?: number
+}
+
+export type GetPetsResponse = {
+    items: IPet[]
+    totalPages: number
+    currentPage: number
+}
+
+
+
+
+// Pick é um atalho
--- src/pages/Pets/Pets.tsx
+++ src/pages/Pets/Pets.tsx
@@ -1,14 +1,20 @@
-import { Link } from "react-router-dom";
 import { Header } from "../../components/common/Header";
 import { Grid } from "../../components/layout/Grid/Grid";
 import styles from "./Pets.module.css";
+import { Card } from "../../components/common/Card";
+import { Skeleton } from "../../components/common/Skeleton";
 
 export function Pets() {
     return ( 
     <Grid> 
         <div className={styles.container}>
         <Header />
-        <Link to="/pets/20">Ir para listagem</Link>
+        <main className={styles.list}>
+            <Skeleton count={5} containerClassName={styles.skeleton}/>
+            <Card href="/pets/1" text="Lilica" thumb="" />
+            <Card href="/pets/2" text="Paçoca" thumb="" />
+            <Card href="/pets/3" text="Paçoquinha" thumb="" />
+        </main>
         </div>
     </Grid>
 )
