f29d7fca073e2a75e62095f35c2ed7f4cd68d08a
SuhSales <suelem_pira@hotmail.com>
2024-04-18 22:27:36 -0300

    Adicionando as páginas no site

--- src/pages/Pets/Pets.tsx
+++ src/pages/Pets/Pets.tsx
@@ -3,19 +3,52 @@ import { Grid } from "../../components/layout/Grid/Grid";
 import styles from "./Pets.module.css";
 import { Card } from "../../components/common/Card";
 import { Skeleton } from "../../components/common/Skeleton";
+import { getPets } from "../../services/pets/getPets";
+import { useQuery } from "@tanstack/react-query";
+import { Pagination } from "../../components/common/Pagination";
+import { useSearchParams } from "react-router-dom";
 
 export function Pets() {
-    return ( 
-    <Grid> 
-        <div className={styles.container}>
-        <Header />
-        <main className={styles.list}>
-            <Skeleton count={5} containerClassName={styles.skeleton}/>
-            <Card href="/pets/1" text="Lilica" thumb="" />
-            <Card href="/pets/2" text="Paçoca" thumb="" />
-            <Card href="/pets/3" text="Paçoquinha" thumb="" />
-        </main>
-        </div>
-    </Grid>
-)
+    const [searchParams, setSearchParams] = useSearchParams()
+
+    const urlParams = {
+        page: searchParams.get('page') ? Number(searchParams.get('page')) : 1,
+    }
+
+    function changePage(page: number) {
+        setSearchParams((params) =>{
+            params.set('page', String(page))
+            return params
+        })
+    }
+
+    return (
+        <Grid>
+            <div className={styles.container}>
+                <Header />
+                {
+                    isLoading && (
+                        <Skeleton containerClassName={styles.skeleton} count={10} />
+                    )}
+                <main className={styles.list}>
+                    {
+                        data?.items.map((pet) => (
+                            <Card
+                                key={pet.id}
+                                href={`/pet/${pet.id}`}
+                                text={pet.name}
+                                thumb={pet.photo}
+                            />
+                        ))}
+                </main>
+                {data?.currentPage && (
+                    <Pagination
+                        currentPage={data.currentPage}
+                        totalPages={data.totalPages}
+                        onPageChange={(number) => changePage(number)}
+                    />
+                )}
+            </div>
+        </Grid>
+    )
 }
--- src/services/pets/getPets.ts
+++ src/services/pets/getPets.ts
@@ -5,11 +5,11 @@ export async function getPets(
     params?: GetPetsRequest
 ): Promise<GetPetsResponse> {
     try {
-    const response = await httpClient.get('/pet', {params})
+        //await new Promise((resolve) => setTimeout(resolve, 2000))
+        const response = await httpClient.get('/pet', { params })
 
-    return response.data
-
-    }catch(error) {
+        return response.data
+    } catch (error) {
         console.log('Erro ao buscar pets')
         throw error
     }
